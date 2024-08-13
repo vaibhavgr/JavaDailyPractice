50f0495457b4f31bce7ea208404a523204f6b228
Vaibhav <grovervaibhav17@gmail.com>
2024-08-13 17:04:59 +0530

    Day 2

---
+++ src/javaPracticeQuestions/Day2.java
@@ -0,0 +1,30 @@
+package javaPracticeQuestions;
+
+//Challenge to Reverse a String in two ways : 
+// Problem 1 : Testing should be replace with gnitseT
+// Problem 2 : my name is vaibhav with vaibhav" with "vahbiav si eman ym"
+// Problem 3 : my name is my name is vaibhav with "vaibhav is name my" 
+public class Day2 {
+
+	public void problemFirst() {
+		
+	}
+
+	public void problemSecond() {
+	}
+
+	public void problemThird() {
+	}
+
+	public void graph() {
+		
+	}	
+	public static void main(String[] args) {
+
+		Day2 StringReverse = new Day2();
+		StringReverse.problemFirst();
+		StringReverse.problemSecond();
+		StringReverse.problemThird();
+		StringReverse.graph();
+	}
+}
